33aa3b5103a4d5eb3b7e13c9c186d39ae7538a23
Радмир Исламов <therad445@outlook.com>
2025-03-15 02:22:38 +0300

    feat(bot): add notification exception handler

---
+++ bot/src/main/java/backend/academy/bot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package backend.academy.bot.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleValidationException(ConstraintViolationException ex) {
+        return ResponseEntity.badRequest().body("Некорректные параметры запроса: " + ex.getMessage());
+    }
+}
