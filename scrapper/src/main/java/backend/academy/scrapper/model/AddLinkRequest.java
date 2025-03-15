e6ce802dc3e86cf769ec3334130b20dd073f2979
Радмир Исламов <therad445@outlook.com>
2025-03-15 22:53:28 +0300

    feat(scrapper): add AddLinkRequest validation @NotEmpty

--- scrapper/src/main/java/backend/academy/scrapper/model/AddLinkRequest.java
+++ scrapper/src/main/java/backend/academy/scrapper/model/AddLinkRequest.java
@@ -1,12 +1,17 @@
 package backend.academy.scrapper.model;
 
 import java.util.Set;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.URL;
 
 public class AddLinkRequest {
     @URL
+    @NotBlank
     private final String link;
+    @NotEmpty
     private final Set<String> tags;
+    @NotEmpty
     private final Set<String> filters;
 
     public AddLinkRequest(String link, Set<String> tags, Set<String> filters) {
