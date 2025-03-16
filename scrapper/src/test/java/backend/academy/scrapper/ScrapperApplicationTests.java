8421442777888b5abd696a4a31e1f541598ab081
Радмир Исламов <therad445@outlook.com>
2025-03-16 19:31:43 +0300

    test(bot): add ScrapperApplicationTests

--- scrapper/src/test/java/backend/academy/scrapper/ScrapperApplicationTests.java
+++ scrapper/src/test/java/backend/academy/scrapper/ScrapperApplicationTests.java
@@ -1,13 +1,30 @@
 package backend.academy.scrapper;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.client.RestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ScrapperApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
+
+    @Test
+    void restTemplateBeanIsPresent() {
+        assertThat(applicationContext.getBean(RestTemplate.class)).isNotNull();
+    }
+
+    @Test
+    void scrapperConfigBeanIsPresent() {
+        assertThat(applicationContext.getBean(ScrapperConfig.class)).isNotNull();
+    }
 }
