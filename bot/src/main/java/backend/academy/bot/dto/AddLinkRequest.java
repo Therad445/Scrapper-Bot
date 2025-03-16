953a1797ddfa73feac8fac103ca65ba17b233a19
Радмир Исламов <therad445@outlook.com>
2025-03-16 18:59:09 +0300

    feat(bot): change AddLinkRequest constructor

--- bot/src/main/java/backend/academy/bot/dto/AddLinkRequest.java
+++ bot/src/main/java/backend/academy/bot/dto/AddLinkRequest.java
@@ -1,11 +1,12 @@
 package backend.academy.bot.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddLinkRequest {
     private String link;
-    private List<String> tags;
-    private List<String> filters;
+    private List<String> tags = new ArrayList<>();
+    private List<String> filters = new ArrayList<>();
 
     public AddLinkRequest() {}
 
