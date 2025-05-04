2241c080d2b3ed2a64e53a5f8ced342c59adbe21
Радмир Исламов <therad445@outlook.com>
2025-05-04 08:09:06 +0300

    feat(scrapper): change LinkJpaRepository

--- scrapper/src/main/java/backend/academy/scrapper/repository/orm/LinkJpaRepository.java
+++ scrapper/src/main/java/backend/academy/scrapper/repository/orm/LinkJpaRepository.java
@@ -1,11 +1,24 @@
 package backend.academy.scrapper.repository.orm;
 
-import java.util.List;
-import java.util.Optional;
+import backend.academy.scrapper.entity.LinkEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.Optional;
+
+@Repository
 public interface LinkJpaRepository extends JpaRepository<LinkEntity, Long> {
-    List<LinkEntity> findByChatId(Long chatId);
-    Optional<LinkEntity> findByChatIdAndLink(Long chatId, String link);
-    void deleteByChatIdAndLink(Long chatId, String link);
+
+    Optional<LinkEntity> findByUrl(String url);
+
+    @Query("""
+        SELECT l FROM LinkEntity l
+         WHERE l.lastCheckedAt < :threshold
+         ORDER BY l.lastCheckedAt
+    """)
+    Page<LinkEntity> findOldLinks(Instant threshold, Pageable pageable);
 }
