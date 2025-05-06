6fbee904e58854ae5938898b56d0f2c00eee7d7e
Радмир Исламов <therad445@outlook.com>
2025-05-06 05:10:35 +0300

    feat(scrapper): add subscription models

---
+++ scrapper/src/main/java/backend/academy/scrapper/model/SubscriptionFilterId.java
@@ -0,0 +1,38 @@
+package backend.academy.scrapper.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionFilterId implements Serializable {
+    @Column(name = "filter_id")
+    private Long filterId;
+
+    @Column(name = "chat_id")
+    private Long chatId;
+
+    @Column(name = "link_id")
+    private Long linkId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SubscriptionFilterId that = (SubscriptionFilterId) o;
+        return Objects.equals(filterId, that.filterId) && Objects.equals(chatId, that.chatId) && Objects.equals(linkId, that.linkId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(filterId, chatId, linkId);
+    }
+}
---
+++ scrapper/src/main/java/backend/academy/scrapper/model/SubscriptionTagId.java
@@ -0,0 +1,38 @@
+package backend.academy.scrapper.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionTagId implements Serializable {
+    @Column(name = "tag_id")
+    private Long tagId;
+
+    @Column(name = "chat_id")
+    private Long chatId;
+
+    @Column(name = "link_id")
+    private Long linkId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SubscriptionTagId that = (SubscriptionTagId) o;
+        return Objects.equals(tagId, that.tagId) && Objects.equals(chatId, that.chatId) && Objects.equals(linkId, that.linkId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagId, chatId, linkId);
+    }
+}
