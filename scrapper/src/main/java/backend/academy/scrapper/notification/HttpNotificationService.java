2728d98ef273849e3ed507ad1012b649530d37d7
Радмир Исламов <therad445@outlook.com>
2025-05-06 10:37:16 +0300

    feat(scrapper): change notification - Rename BotClient to HttpNotificationService - Replace to notification directory - Add NotificationService interface

---
+++ scrapper/src/main/java/backend/academy/scrapper/notification/HttpNotificationService.java
@@ -0,0 +1,42 @@
+package backend.academy.scrapper.notification;
+
+import backend.academy.scrapper.model.LinkUpdate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@ConditionalOnProperty(name = "notification.type", havingValue = "http", matchIfMissing = true)
+public class HttpNotificationService implements NotificationService {
+
+    private final RestTemplate restTemplate;
+
+
+    @Override
+    public void notify(LinkUpdate update) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<LinkUpdate> request = new HttpEntity<>(update, headers);
+
+            String botBaseUrl = "http://bot:8080";
+            restTemplate.postForEntity(
+                botBaseUrl + "/updates",
+                request,
+                Void.class
+            );
+
+            log.info("Отправлено уведомление о ссылке id={}, url={}", update.id(), update.url());
+        } catch (Exception e) {
+            log.error("Ошибка при отправке уведомления", e);
+            throw new IllegalStateException("Не удалось отправить уведомление", e);
+        }
+    }
+}
