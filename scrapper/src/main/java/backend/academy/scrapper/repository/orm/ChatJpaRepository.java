f87cd11e0be158144f0cc9b890108b2be1ff65b0
Радмир Исламов <therad445@outlook.com>
2025-05-06 05:24:08 +0300

    refactor(scrapper): update ChatJpaRepository with subscriptions impl

--- scrapper/src/main/java/backend/academy/scrapper/repository/orm/ChatJpaRepository.java
+++ scrapper/src/main/java/backend/academy/scrapper/repository/orm/ChatJpaRepository.java
@@ -1,21 +1,20 @@
 package backend.academy.scrapper.repository.orm;
 
 import backend.academy.scrapper.entity.ChatEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ChatJpaRepository extends JpaRepository<ChatEntity, Long> {
 
     @Query("""
-        select c.id
-          from ChatEntity c
-          join c.links l
-         where l.id = :linkId
-    """)
+            select c.id
+              from ChatEntity         c
+              join c.subscriptions    s
+             where s.link.id = :linkId
+        """)
     List<Long> findChatIdsByLinkId(@Param("linkId") long linkId);
 }
