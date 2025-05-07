c0d4d4b44b74b54f573183111742e71c281efea5
Радмир Исламов <therad445@outlook.com>
2025-05-07 11:08:13 +0300

    feat(bot): add TelegramBotConfig

---
+++ bot/src/main/java/backend/academy/bot/config/TelegramBotConfig.java
@@ -0,0 +1,16 @@
+package backend.academy.bot.config;
+
+import com.pengrad.telegrambot.TelegramBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfig {
+
+    @Bean
+    public TelegramBot telegramBot(@Value("${app.telegram-token}") String token) {
+        return new TelegramBot(token);
+    }
+}
+
